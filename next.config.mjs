1b60eb60226e8111d393f1bb66fadb0e157d24de
GMALINI <giulio.malini@woodplc.com>
2026-04-02 10:29:14 +0200

    sec: updated policies

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              script-src 'self';
               style-src 'self' 'unsafe-inline';
               img-src 'self' blob: data:;
               font-src 'self';
@@ -18,8 +18,33 @@ const nextConfig = {
               form-action 'self';
               frame-ancestors 'none';
               upgrade-insecure-requests;
+              frame-ancestors 'self';
             `.replace(/\s{2,}/g, ' ').trim(),
           },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          }
         ],
       },
     ]
